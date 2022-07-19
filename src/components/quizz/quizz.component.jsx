7825b4e4e9c0d9add6304b14cacf6abd42593bcc
Golu Cristian <golu.cristian22@gmail.com>
2022-07-19 11:59:58 +0300

    Added The Basic Functionality.

--- src/components/quizz/quizz.component.jsx
+++ src/components/quizz/quizz.component.jsx
@@ -10,26 +10,74 @@ const Quizz = () => {
       .then((data) => setQuestions(data.results));
   }
 
+  function selectAnswer(e) {
+    // Select an answer
+    const selectedAnswer = e.target;
+    // Select all answers from the same question
+    const selectAllQuestionAnswers = document.querySelectorAll(
+      `.${selectedAnswer.classList[1]}`
+    );
+    selectAllQuestionAnswers.forEach((answer) => {
+      answer.classList.remove("question__answer--selected");
+    });
+    selectedAnswer.classList.add("question__answer--selected");
+  }
+
+  function checkAnswers() {
+    // Select all questions
+    const allQuestions = document.querySelectorAll(".quizz__question");
+    for (let i = 0; i < allQuestions.length; i++) {
+      // Select all answers for a question
+      const allQuestionAnswers = document.querySelectorAll(
+        `.answer-to-question-${i}`
+      );
+      // Loop through each answer to check if it's true or false to add the specific class
+      allQuestionAnswers.forEach((questionAnswer) => {
+        if (questionAnswer.classList.contains("question__answer--selected")) {
+          const correctAnswer = questions[i].correct_answer;
+          if (questionAnswer.innerText === correctAnswer) {
+            questionAnswer.classList.add("question__answer--correct");
+          } else {
+            questionAnswer.classList.add("question__answer--wrong");
+            // Loop again through all answers to show the correct one.
+            allQuestionAnswers.forEach((questionAnswer) => {
+              if (questionAnswer.innerText === correctAnswer) {
+                questionAnswer.classList.add("question__answer--correct");
+              }
+            });
+          }
+        }
+      });
+    }
+  }
+
   console.log(questions);
 
   useEffect(getQuestions, []);
 
-  // Get questions answers in an array.
-  // Randomise the array.
-  // Generate the answers.
-  const questionsElements = questions.map((question, index) => {
+  const questionsElements = questions.map((question, questionIndex) => {
     const questionsAnswers = [
       ...question.incorrect_answers,
       question.correct_answer,
     ];
-    console.log(questionsAnswers);
+
+    // Generates random order since the correct answer is always the last.
+    questionsAnswers.sort(() => Math.random() - 0.5);
+
     return (
-      <div className="quizz__question" key={index}>
+      <div
+        className={`quizz__question question-${questionIndex}`}
+        key={questionIndex}
+      >
         <h3 className="question__heading">{question.question}</h3>
         <div className="question__answers-container">
-          {questionsAnswers.map((answer, index) => {
+          {questionsAnswers.map((answer, answerIndex) => {
             return (
-              <div className="question__answer" key={index}>
+              <div
+                className={`question__answer ${`answer-to-question-${questionIndex}`}`}
+                key={answerIndex}
+                onClick={selectAnswer}
+              >
                 {answer}
               </div>
             );
@@ -43,7 +91,7 @@ const Quizz = () => {
     <div className="quizz">
       {questionsElements}
       <div className="quizz__btn-container">
-        <button className="quizz__btn" onClick={getQuestions}>
+        <button className="quizz__btn" onClick={checkAnswers}>
           Check Answers
         </button>
       </div>
